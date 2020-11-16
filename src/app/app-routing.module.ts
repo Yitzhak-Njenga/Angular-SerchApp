edfb50593657509a1eb02a8d10b348e4934d870d
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-11-16 15:50:48 +0300

    added route path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProjectComponent } from './project/project.component';
 
-
+const routes: Routes = [
+  { path: 'project', component:ProjectComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
