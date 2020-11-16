f586654e6949a4158165fed4c25893014c873336
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-11-16 15:23:33 +0300

    upadated files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProjectComponent } from 'Angular-SerchApp/src/app/project/project.component';
+import { ProjectComponent } from './project/project.component';
 
 const routes: Routes = [
   { path: 'project', component:ProjectComponent}
